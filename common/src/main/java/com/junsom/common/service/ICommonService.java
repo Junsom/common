e987990c1957f7ddc09388f3b583f0b61a12c08d
hejunsong <hjsactivity@gmail.com>
2016-04-12 18:52:49 +0800

    初始化提交

---
+++ common/src/main/java/com/junsom/common/service/ICommonService.java
@@ -0,0 +1,76 @@
+package com.junsom.common.service;
+
+import java.util.List;
+
+import com.junsom.common.page.Pageable;
+
+/**
+ * 通用 service
+ * 
+ * @Version: 1.0
+ * @Author: hejunsong 何俊松
+ * @Email: hjsactivity@gmail.com
+ * @Date: 2016年4月11日
+ */
+public interface ICommonService<T> {
+
+  /**
+   * 添加记录
+   * 
+   * @param entity
+   * @return
+   */
+  Integer insert(T entity);
+  
+  /**
+   * 根据id删除记录
+   * 
+   * @param id
+   * @return
+   */
+  Integer deleteById(Long id);
+  
+  /**
+   * 根据id集合删除记录
+   * 
+   * @param ids
+   * @return
+   */
+  Integer deleteByIds(List<Long> ids);
+  
+  /**
+   * 更新记录
+   * 
+   * @param entity
+   * @return
+   */
+  Integer update(T entity);
+
+  /**
+   * 根据主键查找
+   * 
+   * @param id
+   * @return
+   */
+  T selectById(Long id);
+  
+  /**
+   * 根据entity中的值查询, 不包含空值
+   * 
+   * @param entity
+   * @return
+   */
+  List<T> selectByEntityCondition(T entityCondition);
+  
+  /**
+   * 根据实体中的条件分页查询
+   * 
+   * @param entityCondition
+   * @param pageNumber
+   *          查询页
+   * @param pageSize
+   *          每页记录数
+   * @return
+   */
+  List<T> selectByEntityConditionPage(T entityCondition, Pageable pageable);
+}
